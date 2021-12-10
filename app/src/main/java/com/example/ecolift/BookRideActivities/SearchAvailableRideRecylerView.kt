12c22a038f6a875f5f2a108dac4758fa9c7cc1ee
DipeshSingh1772 <dipeshsingh1772@gmail.com>
2021-12-10 11:45:10 +0530

    Search Ride Function and api call added

--- app/src/main/java/com/example/ecolift/BookRideActivities/SearchAvailableRideRecylerView.kt
+++ app/src/main/java/com/example/ecolift/BookRideActivities/SearchAvailableRideRecylerView.kt
@@ -1,4 +1,4 @@
-package com.example.ecolift.MainActivities
+package com.example.ecolift.BookRideActivities
 
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,28 +7,30 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ecolift.Data_Classes.AllPostedRide
+import com.example.ecolift.MainActivities.PostRideListRecyclerView
 import com.example.ecolift.R
 
-class PostRideListRecyclerView: RecyclerView.Adapter<PostRideListRecyclerView.PostListAdapter>() {
+class SearchAvailableRideRecylerView: RecyclerView.Adapter<SearchAvailableRideRecylerView.SearchListAdapter>() {
+
 
     private val allPostRideList = ArrayList<AllPostedRide>()
 
-    inner class PostListAdapter(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class SearchListAdapter(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         val pickup: TextView = itemView.findViewById(R.id.pickup_name)
-        val destination:TextView = itemView.findViewById(R.id.destination_name)
-        val date:TextView = itemView.findViewById(R.id.date)
-        val time:TextView = itemView.findViewById(R.id.time)
-        val seats:TextView = itemView.findViewById(R.id.seats)
-        val amount:TextView = itemView.findViewById(R.id.amount)
+        val destination: TextView = itemView.findViewById(R.id.destination_name)
+        val date: TextView = itemView.findViewById(R.id.date)
+        val time: TextView = itemView.findViewById(R.id.time)
+        val seats: TextView = itemView.findViewById(R.id.seats)
+        val amount: TextView = itemView.findViewById(R.id.amount)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostListAdapter {
-        val viewHolder = PostListAdapter(LayoutInflater.from(parent.context).inflate(R.layout.allposted_ride_recyclerview_format,parent,false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchListAdapter {
+        val viewHolder = SearchListAdapter(LayoutInflater.from(parent.context).inflate(R.layout.allposted_ride_recyclerview_format,parent,false))
         return viewHolder
     }
 
-    override fun onBindViewHolder(holder:PostListAdapter, position: Int) {
+    override fun onBindViewHolder(holder:SearchListAdapter, position: Int) {
         val positionOnScreen = allPostRideList[position]
         holder.pickup.text = "Pickup: " + positionOnScreen.pickup
         holder.destination.text = "Destination: " +   positionOnScreen.destination
